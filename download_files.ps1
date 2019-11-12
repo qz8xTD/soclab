229e8a875ad358b5827737e165b4b9bb93ddad7e
Ted <ted@grupp.xyz>
2019-11-12 04:52:24 -0700

    test of complete setup

--- download_files.ps1
+++ download_files.ps1
@@ -1,12 +1,21 @@
 mkdir 'C:\dc1_setup' -ErrorAction SilentlyContinue
 
-$guest_additions_uri = "https://github.com/qz8xTD/soclab/raw/master/guest_additions.zip"
+$guest_additions_uri = "https://raw.githubusercontent.com/qz8xTD/soclab/master/guest_additions.ps1"
+$guest_additions_zip_uri = "https://github.com/qz8xTD/soclab/raw/master/guest_additions.zip"
 $preboot_uri = "https://raw.githubusercontent.com/qz8xTD/soclab/master/dc1_preboot.ps1"
 $postreboot_uri = "https://raw.githubusercontent.com/qz8xTD/soclab/master/dc1_postreboot.ps1"
 
-Invoke-WebRequest -Uri $guest_additions_uri  -OutFile 'C:\dc1_setup\guest_additions.zip'
+Invoke-WebRequest -Uri $guest_additions_uri -OutFile 'C:\dc1_setup\guest_additions.ps1'
+Invoke-WebRequest -Uri $guest_additions_zip_uri  -OutFile 'C:\dc1_setup\guest_additions.zip'
 Invoke-WebRequest -Uri $preboot_uri -OutFile 'C:\dc1_setup\dc1_preboot.ps1'
 Invoke-WebRequest -Uri $postreboot_uri -OutFile 'C:\dc1_setup\dc1_postreboot.ps1'
 
+Unblock-File 'C:\dc1_setup\guest_additions.ps1'
 Unblock-File 'C:\dc1_setup\dc1_preboot.ps1'
 Unblock-File 'C:\dc1_setup\dc1_postreboot.ps1'
+
+Set-Location -Path 'C:\dc1_setup'
+.\guest_additions.ps1
+
+Set-Location -Path 'C:\dc1_setup'
+.\dc1_preboot.ps1
