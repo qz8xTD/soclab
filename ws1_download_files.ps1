da3252fb6ad75803fe5b539987b535c5b3ab6dcf
Ted <ted@grupp.xyz>
2019-11-13 04:59:31 -0700

    added preboot ps1

--- ws1_download_files.ps1
+++ ws1_download_files.ps1
@@ -2,10 +2,12 @@ mkdir 'C:\ws1_setup' -ErrorAction SilentlyContinue
 
 $guest_additions_uri = "https://raw.githubusercontent.com/qz8xTD/soclab/master/ws1_guest_additions.ps1"
 $guest_additions_zip_uri = "https://github.com/qz8xTD/soclab/raw/master/guest_additions.zip"
+$preboot_uri = "https://raw.githubusercontent.com/qz8xTD/soclab/master/ws1_preboot.ps1"
 
 
 Invoke-WebRequest -Uri $guest_additions_uri -OutFile 'C:\ws1_setup\guest_additions.ps1'
 Invoke-WebRequest -Uri $guest_additions_zip_uri  -OutFile 'C:\ws1_setup\guest_additions.zip'
+Invoke-WebRequest -Uri $preboot_uri -OutFile 'C:\ws1_setup\ws1_preboot.ps1'
 
 
 Unblock-File "C:\ws1_setup\*"
