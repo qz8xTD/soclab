6131f1d4e8903d27ce9a52c194d5b45fb41b5a79
Ted <ted@grupp.xyz>
2019-11-13 06:27:55 -0700

    commented out psremoting

--- ws1_preboot.ps1
+++ ws1_preboot.ps1
@@ -7,10 +7,10 @@ $interfaceAlias = "ethernet"
 $gateway = '192.168.1.1'
 $subnetMask = '24'
 
-#Rename-Computer -NewName $computerName -Force -Confirm:$false
 Set-TimeZone -Name $timezone
 New-NetIPAddress -InterfaceAlias $interfaceAlias -IPAddress $ipAddress -PrefixLength $subnetMask -DefaultGateway $gateway -AddressFamily IPv4
 Set-DnsClientServerAddress -InterfaceAlias $interfaceAlias -ServerAddresses $dnsServer
-Enable-PSRemoting -Force
+# Causing error because it is not part of a domain yet. Cannot ignore error.
+#Enable-PSRemoting -Force
 Enable-NetFirewallRule -DisplayGroup 'File and Printer Sharing'
 Add-Computer -NewName $computerName -DomainName $domainName -Credential (Get-Credential) -Restart
